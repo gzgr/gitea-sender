f430694a9b5d409673ecf194dc773c70fc4111e0
guzig <guzig@cyclonopedia.com>
2024-08-12 21:49:58 +0900

    add files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Gitea Sender
+
+준비물
+
+1. 키파일
+
+절차
+
+1. git에 commit된 리스트 확인
+2. git pull을 하고 해당 commit된 파일들 리스트를 확인해서 폴더별로 압축
+3. 압축 후 ftp로 전송
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,56 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use env_logger;
+use log::info;
+use serde::Serialize;
+use serde_json::Value;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Serialize)]
+struct HealthResponse {
+    status: String,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+async fn health() -> impl Responder {
+    let response = HealthResponse {
+        status: "ok".to_string(),
+    };
+    HttpResponse::Ok().json(response)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn webhook_handler(payload: web::Json<Value>) -> impl Responder {
+    info!("Received webhook request");
+
+    // Webhook으로 전달된 JSON 데이터를 파싱하여 필요한 값을 추출
+    if let Some(commits) = payload.get("commits").and_then(|c| c.as_array()) {
+        for commit in commits {
+            if let Some(added_files) = commit.get("added").and_then(|a| a.as_array()) {
+                let added_files_vec: Vec<String> = added_files
+                    .iter()
+                    .filter_map(|v| v.as_str().map(String::from))
+                    .collect();
+
+                // 추출한 벡터를 로그로 출력
+                info!("Added files: {:?}", added_files_vec);
+            }
+        }
+    }
+
+    // 요청에 대해 HTTP 200 OK 응답
+    HttpResponse::Ok().body("Webhook processed")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // 로깅 초기화 및 로그 레벨 설정
+    env_logger::Builder::from_default_env()
+        .filter(None, log::LevelFilter::Info)
+        .init();
+
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/health", web::get().to(health))
+            .route("/webhook", web::post().to(webhook_handler))
     })
-    .bind(("0.0.0.0", 18080))?
+    .bind("0.0.0.0:10080")?
     .run()
     .await
 }
